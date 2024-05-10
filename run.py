bc5cd4aeefe43dc4d0810d4cc61732806b1c1fd4
syhmyhy <syahmiyahya13@gmail.com>
2024-05-10 12:20:45 +0800

    setup this pc as server, add virtual environment in this pc, modify line code in run.py

--- run.py
+++ run.py
@@ -3,4 +3,8 @@
 from app import app, db
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Replace 'your_server_ip' with your actual server's IP address
+    server_ip = '192.168.1.106'  # Example IP address
+    
+    # Run the Flask app with debug mode enabled
+    app.run(host=server_ip, port=5000, debug=True)
