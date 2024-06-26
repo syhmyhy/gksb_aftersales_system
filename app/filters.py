9a57609f9c631f729d86a33efbce1d42ff934a01
syhmyhy <syahmiyahya13@gmail.com>
2024-06-26 16:59:22 +0800

    change format

--- app/filters.py
+++ app/filters.py
@@ -1,8 +1,4 @@
 # app/filters.py
 
-import locale
-
 def format_rm_currency(value):
-    # Set the locale to Malaysian Ringgit (Bahasa Malaysia)
-    locale.setlocale(locale.LC_ALL, 'ms_MY.UTF-8')  # Set locale to Malaysian Ringgit (Bahasa Malaysia)
-    return locale.currency(value, grouping=True, symbol='RM')
+    return f"RM{value:,.2f}"
